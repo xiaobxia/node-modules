af57ddc3d72a8cafbee2792e493d0044ee6745cc
chenlingjie <chenlingjie@cd121.com>
2017-07-07 14:02:56 +0800

    添加官方例子

---
+++ qiniu/official_examples/resume_upload_simple.js
@@ -0,0 +1,41 @@
+const qiniu = require("qiniu");
+const proc = require("process");
+
+var bucket = 'if-pbl';
+var accessKey = proc.env.QINIU_ACCESS_KEY;
+var secretKey = proc.env.QINIU_SECRET_KEY;
+var mac = new qiniu.auth.digest.Mac(accessKey, secretKey);
+var options = {
+  scope: bucket,
+}
+var putPolicy = new qiniu.rs.PutPolicy(options);
+
+var uploadToken = putPolicy.uploadToken(mac);
+var config = new qiniu.conf.Config();
+var localFile = "/Users/jemy/Documents/qiniu.mp4";
+config.zone = qiniu.zone.Zone_z0;
+config.useCdnDomain = true;
+var resumeUploader = new qiniu.resume_up.ResumeUploader(config);
+var putExtra = new qiniu.resume_up.PutExtra();
+putExtra.params = {
+  "x:name": "",
+  "x:age": 27,
+}
+putExtra.fname = 'testfile.mp4';
+// 这个上传进度记录文件的名字需要和上传的文件相关联，不可以多个文件共享一个记录文件
+putExtra.resumeRecordFile = 'progress.log';
+
+//file
+resumeUploader.putFile(uploadToken, null, localFile, putExtra, function(respErr,
+  respBody, respInfo) {
+  if (respErr) {
+    throw respErr;
+  }
+
+  if (respInfo.statusCode == 200) {
+    console.log(respBody);
+  } else {
+    console.log(respInfo.statusCode);
+    console.log(respBody);
+  }
+});
