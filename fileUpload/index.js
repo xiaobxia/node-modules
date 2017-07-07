a595daf622ed915ffb6c94c447f5166003152046
chenlingjie <chenlingjie@cd121.com>
2017-07-07 14:57:32 +0800

    上传文件

---
+++ fileUpload/index.js
@@ -0,0 +1,31 @@
+/**
+ * Created by xiaobxia on 2017/7/7.
+ */
+var express = require('express')
+var app = express()
+var multiparty = require('multiparty')
+
+let form = new multiparty.Form();
+//form.encoding = 'utf-8';
+//设置文件存储路径
+form.uploadDir = "uploads/";
+//设置单文件大小限制
+//form.maxFilesSize = 2 * 1024 * 1024;
+app.post('/profile', function (req, res, next) {
+    form.parse(req, function(err, fields, files) {
+        console.log(err)
+        console.log(fields)
+        console.log(files)
+        res.end('ok')
+    })
+});
+
+//启动服务器
+module.exports = app.listen(4000, function (err) {
+    if (err) {
+        logger.debug(err)
+        return
+    }
+    let uri = 'http://localhost:' +4000;
+    console.log('Listening at ' + uri)
+});
