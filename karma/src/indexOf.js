e1bbb6765b7f19697ce6406ed1c830b6f6544df3
chenlingjie <chenlingjie@cd121.com>
2017-08-24 14:33:31 +0800

    karma的用例

---
+++ karma/src/indexOf.js
@@ -0,0 +1,6 @@
+/**
+ * Created by xiaobxia on 2017/8/24.
+ */
+var indexOf = function (array, index) {
+    return array[index];
+};
---
+++ karma/test/indexOf.spec.js
@@ -0,0 +1,14 @@
+/**
+ * Created by xiaobxia on 2017/8/24.
+ */
+//这里能用chai是因为在配置的frameworks中加了chai
+//当前的环境就是浏览器的环境，所以不能用es6和模块系统
+describe('indexOf', function () {
+    //it声明测试实例
+    it('["a","b"]索引1的位置是"b"', function () {
+        expect(indexOf(['a', 'b'], 1)).to.be.equal('b');
+    });
+    it('["a","b"]索引2的位置是undefined', function () {
+        expect(indexOf(['a', 'b'], 2)).to.be.equal(undefined);
+    })
+});
