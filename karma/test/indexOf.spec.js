5d6db5e5fe66a291bcb9fbbb2544d22320d91732
chenlingjie <chenlingjie@cd121.com>
2017-08-25 09:33:01 +0800

    修改注释

--- karma/test/indexOf.spec.js
+++ karma/test/indexOf.spec.js
@@ -2,6 +2,8 @@
  * Created by xiaobxia on 2017/8/24.
  */
 //这里能用chai是因为在配置的frameworks中加了chai
+//需要karma start karma/karma.config.js启动
+
 //当前的环境就是浏览器的环境，所以不能用es6和模块系统
 describe('indexOf', function () {
   //it声明测试实例
